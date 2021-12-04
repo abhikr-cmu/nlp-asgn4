732c47838f4db044fff92d81b9cfaaace30eef6d
Abhishek Kumar <ak5@andrew.cmu.edu>
2021-12-03 22:29:57 -0500

    Added a script to sample from ES

---
+++ resample.py
@@ -0,0 +1,24 @@
+import csv
+import numpy as np
+import sys
+from elasticsearch import Elasticsearch
+import json
+
+tsv_file = open(sys.argv[1])
+read_tsv = csv.reader(tsv_file, delimiter="\t")
+
+qids = []
+prob = []
+
+for row in read_tsv:
+    qids.append(row[0])
+    prob.append(row[1])
+
+sampled_qids = np.random.choice(qids, size=10000,  p=prob, replace=True)
+
+es = Elasticsearch()
+
+with open(sys.argv[2], 'w') as wf:
+    for sq in sampled_qids:
+        si = es.search(index="python-code", q="question_id:{}")['hits']['hits'][0]['_source']
+        wf.write(json.dumps(si) + "\n")
