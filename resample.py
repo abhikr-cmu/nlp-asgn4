a54765adc56e9df7809b4eaad4da96a263f3ea5b
Ubuntu <ubuntu@ip-172-31-8-188.us-east-2.compute.internal>
2021-12-06 02:14:30 +0000

    Changes for limiting intent length and adding library name

--- resample.py
+++ resample.py
@@ -12,7 +12,15 @@ prob = []
 
 for row in read_tsv:
     qids.append(row[0])
-    prob.append(row[1])
+    prob.append(float(row[1]))
+
+p_sum = 0.0
+
+for p in prob:
+    p_sum += p
+
+for i in range(len(prob)):
+    prob[i] = prob[i]/p_sum
 
 sampled_qids = np.random.choice(qids, size=10000,  p=prob, replace=True)
 
@@ -20,5 +28,7 @@ es = Elasticsearch()
 
 with open(sys.argv[2], 'w') as wf:
     for sq in sampled_qids:
-        si = es.search(index="python-code", q="question_id:{}")['hits']['hits'][0]['_source']
-        wf.write(json.dumps(si) + "\n")
+        si = es.search(index="python-code", q="question_id:{}".format(sq))['hits']['hits'][0]['_source']
+        wf.write(json.dumps(si) + ",\n")
+
+
